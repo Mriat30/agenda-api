1efba4c932ff87a088ec82f30f7f8f35f93fabe3
Mateo <mriat@fi.uba.ar>
2025-12-17 11:55:50 -0300

    feat: agrego lso servicios al pipeline, modifico el dockerfile

---
+++ tests/users/infrastructure/user-repository/prisma-user-repository.test.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../../../../src/infraestructure/db/prisma";
+import { PrismaUserRepository } from "../../../../src/users/infrastructure/user-repository/prisma-user-repository";
+
+describe("PrismaUserRepository Integration", () => {
+  let repository: PrismaUserRepository;
+
+  beforeAll(() => {
+    repository = new PrismaUserRepository();
+  });
+
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  describe("getById", () => {
+    it("debe recuperar un usuario insertado en la DB real", async () => {
+      // 1. ARRANGE: Insertamos datos directamente en la DB (Bypaseando el repositorio)
+      const newUser = await prisma.user.create({
+        data: {
+          email: "test@example.com",
+          name: "Usuario Test",
+          // Asegúrate de agregar campos obligatorios si tu schema los pide (ej. password)
+        },
+      });
+
+      const foundUser = await repository.getById(newUser.id);
+
+      expect(foundUser).toBeDefined();
+      expect(foundUser?.id).toBe(newUser.id);
+      expect(foundUser?.email).toBe("test@example.com");
+    });
+  });
+});

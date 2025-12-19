b9904b1baa800c0f329943e43509660ea3fce9f5
Mateo <mriat@fi.uba.ar>
2025-12-19 20:35:41 -0300

    feat: agrego el jon de test, cambio el dockerfile.prod para poder usarlo en render

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,25 +2,33 @@ import "dotenv/config";
 
 export class Configuration {
   static getDatabaseUrl(): string {
-    const mode = process.env.NODE_ENV || "development";
+    const nodeEnv = process.env.NODE_ENV || "development";
+    const dbUrl = process.env.DATABASE_URL;
+    const testDbUrl = process.env.TEST_DB_URL;
 
-    switch (mode) {
-      case "test":
-        return (
-          process.env.TEST_DB_URL ||
-          "postgresql://test_user:test_password@localhost:5433/test_db?schema=public"
-        );
-
-      case "production":
-        if (!process.env.DATABASE_URL)
-          throw new Error("DATABASE_URL is missing in production");
-        return process.env.DATABASE_URL;
-
-      default:
-        return (
-          process.env.DATABASE_URL ||
-          "postgresql://dev_user:dev_pass@localhost:5432/dev_db?schema=public"
-        );
+    if (nodeEnv === "test") {
+      return (
+        testDbUrl ||
+        "postgresql://test_user:test_password@localhost:5433/test_db?schema=public"
+      );
     }
+
+    if (nodeEnv === "production") {
+      if (!dbUrl) {
+        throw new Error(
+          "CRITICAL: DATABASE_URL is missing in production mode!"
+        );
+      }
+      return dbUrl;
+    }
+
+    return (
+      dbUrl ||
+      "postgresql://dev_user:dev_pass@localhost:5432/dev_db?schema=public"
+    );
+  }
+
+  static getPort(): number {
+    return Number(process.env.PORT) || 3000;
   }
 }

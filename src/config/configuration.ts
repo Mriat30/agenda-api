997cb448703d8c04e9ac4d9689d5237c2a339b95
Mateo <mriat@fi.uba.ar>
2025-12-22 15:15:53 -0300

    fix: cambio huksy para que corra correctamente

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,30 +3,19 @@ import "dotenv/config";
 export class Configuration {
   static getDatabaseUrl(): string {
     const nodeEnv = process.env.NODE_ENV || "development";
-    const dbUrl = process.env.DATABASE_URL;
-    const testDbUrl = process.env.TEST_DB_URL;
 
-    if (nodeEnv === "production") {
-      if (!dbUrl)
-        throw new Error(
-          "CRITICAL: DATABASE_URL is missing in production environment"
-        );
-      return dbUrl;
+    if (process.env.DATABASE_URL) {
+      return process.env.DATABASE_URL;
     }
 
     if (nodeEnv === "test") {
       return (
-        testDbUrl ||
-        "postgresql://webapi:Passw0rd!@localhost:5433/db_test?schema=public"
+        process.env.TEST_DB_URL ||
+        "postgresql://test_user:test_password@localhost:5433/test_db?schema=public"
       );
     }
 
-    // 3. DESARROLLO LOCAL (Docker Compose)
-    // Si no hay nada de lo anterior, usamos la de desarrollo.
-    return (
-      dbUrl ||
-      "postgresql://dev_user:dev_pass@localhost:5432/dev_db?schema=public"
-    );
+    return "postgresql://dev_user:dev_password@localhost:5432/dev_db?schema=public";
   }
 
   static getPort(): number {

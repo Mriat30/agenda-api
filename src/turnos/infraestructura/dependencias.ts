93dd72ad8a405a2b0ccddb3ae5bd8fd37632cad6
Mateo <mriat@fi.uba.ar>
2026-01-12 21:25:05 +0000

    feat: agrego correctamente a dependencias la exportacion de turnos controlador

--- src/turnos/infraestructura/dependencias.ts
+++ src/turnos/infraestructura/dependencias.ts
@@ -1,9 +1,8 @@
 import { AgendarTurnoUnico } from "../aplicacion/agendar-turno-unico";
 import { PrismaTurnosRepositorio } from "./repositorio/prisma-turnos-repositorio";
+import { TurnosControlador } from "./rest-api/turnos-controlador";
 
 const turnosRepositorio = new PrismaTurnosRepositorio();
 const agendarTurnoUnico = new AgendarTurnoUnico(turnosRepositorio);
 
-export const turnosController = {
-  agendarTurnoUnico,
-};
+export const turnosController = new TurnosControlador(agendarTurnoUnico);

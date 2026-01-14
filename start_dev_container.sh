eee43667abc9b8402298e12b48bbca6ce750133b
Mateo <mriat@fi.uba.ar>
2026-01-14 14:11:34 +0000

    refactor: cambio el start_dev_container.sh

--- start_dev_container.sh
+++ start_dev_container.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
+
+if ! docker network inspect localdev >/dev/null 2>&1; then
+  echo "Creando red localdev..."
+  docker network create localdev
+fi
+
 docker compose up -d
 docker compose exec app /bin/bash
